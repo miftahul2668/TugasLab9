4a783b4bb30144ad8176ebcd58d9d6fe69e1c9de
hoiru <miftahul.01@students.amikom.ac.id>
2020-06-30 00:10:24 +0700

    Tugas_Lab9

---
+++ ClassInduk/Karyawan.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PolymorphCollection_ClassInduk
+{
+    abstract public class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public string Tipe { get; set; }
+        public abstract double Gaji();
+    }
+}
